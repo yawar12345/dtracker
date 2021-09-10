56930a53b6f2ca82f2356839f61bd0c9fba2d3a6
yawar12345 <40006106+yawar12345@users.noreply.github.com>
2021-09-10 15:12:17 +0500

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -59,5 +59,6 @@ for url in Lines:
     my_string = '\n'.join(map(str, stock_count)) 
     print(my_string)
     requests.get("https://api.telegram.org/bot1987340011:AAGQVebsO9YguKsrGMWoSolnvI2jRwfJeik/sendMessage?chat_id=447905121&text="+my_string)
+    requests.get("https://api.telegram.org/bot1987340011:AAGQVebsO9YguKsrGMWoSolnvI2jRwfJeik/sendMessage?chat_id=1982455844&text="+my_string)
 
     print('Success')
